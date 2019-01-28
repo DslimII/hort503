55d39cd65b1c49bc505936fa236da483df08e420
DslimII <david.sliman@wsu.edu>
2019-01-27 20:01:38 -0800

    Assignment04 First Draft

---
+++ Assignment04/ex27.py
@@ -0,0 +1,13 @@
+#The Truth Terms
+#In Python we have the following terms (characters and phrases) for determining if something is ”True”
+#or ”False.” Logic on a computer is all about seeing if some combination of these characters and some
+#variables is True at that point in the program.
+#• and
+#• or
+#• not
+#• != (not equal)
+#• == (equal)
+#• >= (greater-than-equal)
+#• <= (less-than-equal)
+#• True
+#• False
